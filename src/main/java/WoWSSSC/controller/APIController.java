51b6d21f3f58b0a795792e5fd535881e5531537f
Keun-Ho Lee <khl@qualson.com>
2016-11-18 18:58:40 +0900

    API Importer

--- src/main/java/WoWSSSC/controller/APIController.java
+++ src/main/java/WoWSSSC/controller/APIController.java
@@ -1,5 +1,6 @@
 package WoWSSSC.controller;
 
+import WoWSSSC.model.warships.Ship;
 import WoWSSSC.service.APIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,7 +26,11 @@ public class APIController
     }
 
     @RequestMapping (value = "/", method = RequestMethod.GET)
-    public String test(Model model,
+    public String test(
+            Model model,
+            @RequestParam(required = false, defaultValue = "") String nation,
+            @RequestParam(required = false, defaultValue = "") String shipType,
+            @RequestParam(required = false, defaultValue = "") String ship,
             @RequestParam(required = false, defaultValue = "") String ship_id,
             @RequestParam(required = false, defaultValue = "") String artillery_id,
             @RequestParam(required = false, defaultValue = "") String dive_bomber_id,
@@ -36,9 +41,20 @@ public class APIController
             @RequestParam(required = false, defaultValue = "") String hull_id,
             @RequestParam(required = false, defaultValue = "") String torpedo_bomber_id,
             @RequestParam(required = false, defaultValue = "") String torpedoes_id
-            )
+        )
     {
+
+        if (!nation.equals("") && !shipType.equals("") && !ship.equals(""))
+        {
+            LinkedHashMap<String, LinkedHashMap> shipTypes = (LinkedHashMap<String, LinkedHashMap>) apiService.getData().get("nations").get(nation);
+            LinkedHashMap<String, Ship> ships = shipTypes.get(shipType);
+            Ship warship = ships.get(ship);
+
+            model.addAttribute("warship", warship);
+        }
+
         model.addAttribute("data", apiService.getData());
+
         return "home";
     }
 }
