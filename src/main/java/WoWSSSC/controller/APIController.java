a80970499de2ef07936306bc3a4291082e5703e9
BlindNImpotent <ghostandarkness@gmail.com>
2016-11-19 12:39:29 +0900

    API test

--- src/main/java/WoWSSSC/controller/APIController.java
+++ src/main/java/WoWSSSC/controller/APIController.java
@@ -1,13 +1,12 @@
 package WoWSSSC.controller;
 
-import WoWSSSC.model.shipprofile.ShipData;
+import WoWSSSC.model.shipprofile.Ship;
 import WoWSSSC.model.warships.Warship;
 import WoWSSSC.service.APIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.LinkedHashMap;
 
@@ -20,11 +19,6 @@ public class APIController
     @Autowired
     private APIService apiService;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private Warship warship;
-
     @ResponseBody
     @RequestMapping (value = "/test", method = RequestMethod.GET)
     public LinkedHashMap<String, LinkedHashMap> data()
@@ -33,32 +27,21 @@ public class APIController
     }
 
     @RequestMapping (value = "/", method = RequestMethod.GET)
-    public String home(Model model)
-    {
-        model.addAttribute("data", apiService.getData());
-        model.addAttribute("warship", warship);
-        return "home";
-    }
-
-    @ResponseBody
-    @RequestMapping (value = "/", method = RequestMethod.POST)
-    public void getWarship(
+    public String home(
+            Model model,
             @RequestParam(required = false, defaultValue = "") String nation,
             @RequestParam(required = false, defaultValue = "") String shipType,
             @RequestParam(required = false, defaultValue = "") String ship
     )
     {
-        if (!nation.equals("") && !shipType.equals("") && !ship.equals(""))
-        {
-            LinkedHashMap<String, LinkedHashMap> shipTypes = (LinkedHashMap<String, LinkedHashMap>) apiService.getData().get("nations").get(nation);
-            LinkedHashMap<String, Warship> warships = shipTypes.get(shipType);
-            warship = warships.get(ship);
-        }
+        model.addAttribute("data", apiService.getData());
+        model.addAttribute("warship", apiService.getWarship(nation, shipType, ship));
+        return "home";
     }
 
     @ResponseBody
-    @RequestMapping (value = "/shipAPI", method = RequestMethod.GET)
-    public ShipData getShipParameterAPI(
+    @RequestMapping (value = "/shipAPI", method = RequestMethod.POST)
+    public Ship getShipParameterAPI(
             @RequestParam(required = false, defaultValue = "") String ship_id,
             @RequestParam(required = false, defaultValue = "") String artillery_id,
             @RequestParam(required = false, defaultValue = "") String dive_bomber_id,
@@ -71,7 +54,6 @@ public class APIController
             @RequestParam(required = false, defaultValue = "") String torpedoes_id
     )
     {
-        String url = "https://api.worldofwarships.com/wows/encyclopedia/shipprofile/?application_id=137f0721e1b1baf30d6dcd1968fc260c&artillery_id=3433017136&ship_id=4180621104&hull_id=3443273520";
-        return restTemplate.getForObject(url, ShipData.class);
+        return apiService.getShipAPI(ship_id, artillery_id, dive_bomber_id, engine_id, fighter_id, fire_control_id, flight_control_id, hull_id, torpedo_bomber_id, torpedoes_id);
     }
 }
--- src/main/java/WoWSSSC/service/APIService.java
+++ src/main/java/WoWSSSC/service/APIService.java
@@ -1,8 +1,14 @@
 package WoWSSSC.service;
 
+import WoWSSSC.model.shipprofile.Ship;
+import WoWSSSC.model.shipprofile.ShipData;
+import WoWSSSC.model.warships.Warship;
 import WoWSSSC.parser.AsyncHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 
@@ -12,12 +18,51 @@ import java.util.*;
 @Service
 public class APIService
 {
-   @Autowired
+    @Autowired
     private AsyncHashMap asyncHashMap;
+
+    @Autowired
+    private String APP_ID;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final Logger logger = LoggerFactory.getLogger(APIService.class);
 
     public LinkedHashMap<String, LinkedHashMap> getData()
     {
         return asyncHashMap.getData();
+    }
+
+    public Warship getWarship(String nation, String shipType, String ship)
+    {
+        if (!nation.equals("") && !shipType.equals("") && !ship.equals(""))
+        {
+            LinkedHashMap<String, LinkedHashMap> shipTypes = (LinkedHashMap<String, LinkedHashMap>) asyncHashMap.getData().get("nations").get(nation);
+            LinkedHashMap<String, Warship> warships = shipTypes.get(shipType);
+            return warships.get(ship);
+        }
+        return null;
+    }
+
+    public Ship getShipAPI(
+            String ship_id,
+            String artillery_id,
+            String dive_bomber_id,
+            String engine_id,
+            String fighter_id,
+            String fire_control_id,
+            String flight_control_id,
+            String hull_id,
+            String torpedo_bomber_id,
+            String torpedoes_id
+    )
+    {
+        String url = "https://api.worldofwarships.com/wows/encyclopedia/shipprofile/?application_id=" + APP_ID + "&ship_id=" + ship_id + "&artillery_id=" + artillery_id + "&dive_bomber_id=" + dive_bomber_id + "&engine_id=" + engine_id
+                + "&fighter_id=" + fighter_id + "&fire_control_id=" + fire_control_id + "&flight_control_id=" + flight_control_id + "&hull_id=" + hull_id + "&torpedo_bomber_id=" + torpedo_bomber_id + "&torpedoes_id=" + torpedoes_id;
+        logger.info(url);
+        ShipData shipData = restTemplate.getForObject(url, ShipData.class);
+        return shipData.getData().get(ship_id);
     }
 
 }
