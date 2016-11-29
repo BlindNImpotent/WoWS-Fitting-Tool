a38cb22068a0f3090f16d5d5af20650c4a49df5b
Keun-Ho Lee <khl@qualson.com>
2016-11-29 18:33:35 +0900

    API Importer - Redirect

--- src/main/java/WoWSSSC/controller/APIController.java
+++ src/main/java/WoWSSSC/controller/APIController.java
@@ -1,5 +1,6 @@
 package WoWSSSC.controller;
 
+import WoWSSSC.model.warships.Warship;
 import WoWSSSC.service.APIService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -52,10 +55,12 @@ public class APIController
         return "home";
     }
 
-    @RequestMapping (value = "/warship", method = RequestMethod.GET)
+    @RequestMapping (value = "/warship", method = { RequestMethod.GET, RequestMethod.POST })
     public String getWarship
             (
+                    HttpServletRequest request,
                     Model model,
+                    RedirectAttributes redirectAttributes,
                     @RequestParam(required = false, defaultValue = "") String nation,
                     @RequestParam(required = false, defaultValue = "") String shipType,
                     @RequestParam(required = false, defaultValue = "") String ship
@@ -65,9 +70,17 @@ public class APIController
         {
             logger.info("Loading " + nation + " " + shipType + " " + ship);
             model.addAttribute("warship", ((LinkedHashMap<String, LinkedHashMap>) data.get("nations").get(nation)).get(shipType).get(ship));
-        }
 
-        return "warshipPage :: warshipStats";
+            if (request.getMethod().equalsIgnoreCase("POST"))
+            {
+                return "warshipPage :: warshipStats";
+            }
+            else
+            {
+                redirectAttributes.addFlashAttribute("warship", ((LinkedHashMap<String, LinkedHashMap>) data.get("nations").get(nation)).get(shipType).get(ship));
+            }
+        }
+        return "redirect:";
     }
 
     @RequestMapping (value = "/shipAPI", method = RequestMethod.POST)
