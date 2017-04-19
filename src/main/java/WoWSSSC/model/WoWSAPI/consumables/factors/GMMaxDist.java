e1866413097af11ea6fc7d760fc7de4bb19f06f0
BlindNImpotent <ghostandarkness@gmail.com>
2017-04-20 07:36:19 +0900

    API Importer - Working on Consumables

---
+++ src/main/java/WoWSSSC/model/WoWSAPI/consumables/factors/GMMaxDist.java
@@ -0,0 +1,13 @@
+package WoWSSSC.model.WoWSAPI.consumables.factors;
+
+import lombok.Data;
+
+/**
+ * Created by Aesis on 2017-04-20.
+ */
+@Data
+public class GMMaxDist
+{
+    private String description;
+    private float value;
+}
