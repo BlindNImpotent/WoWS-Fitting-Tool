6563286771b4595f3a9efe8a98408f0aa3d3df52
BlindNImpotent <ghostandarkness@gmail.com>
2017-05-21 23:31:26 +0900

    API Importer - Force SSL

---
+++ src/main/java/WoWSSSC/config/CustomFilter.java
@@ -0,0 +1,65 @@
+//package WoWSSSC.config;
+//
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// * Created by Aesis on 2017. 5. 21..
+// */
+//@Configuration
+//@ComponentScan(basePackages = {"WoWSSSC"})
+//public class CustomFilter implements Filter
+//{
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException
+//    {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+//    {
+//        HttpServletRequest request = (HttpServletRequest) req;
+//        HttpServletResponse response = (HttpServletResponse) res;
+//
+//        StringBuilder https = new StringBuilder("https://");
+//        String serverName = request.getServerName();
+//        String uri = request.getRequestURI();
+//        String queryString = request.getQueryString();
+//
+//        String protoHeader = request.getHeader("X-Forwarded-Proto");
+//        if ("http".equalsIgnoreCase(protoHeader))
+//        {
+//            if (StringUtils.isNotEmpty(serverName))
+//            {
+//                https.append(serverName);
+//            }
+//
+//            if (StringUtils.isNotEmpty(uri))
+//            {
+//                https.append(uri);
+//            }
+//
+//            if (StringUtils.isNotEmpty(queryString))
+//            {
+//                https.append(queryString);
+//            }
+//
+//            response.sendRedirect(https.toString());
+//        }
+//
+//        chain.doFilter(req, res);
+//    }
+//
+//    @Override
+//    public void destroy()
+//    {
+//
+//    }
+//}
