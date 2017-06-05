72ef016d66e8f95ea4b403ebfaabbe2381b42ac5
Keun-Ho Lee <khl@qualson.com>
2017-06-05 12:19:34 +0900

    API Importer - Discord Webhook

--- src/main/java/WoWSSSC/config/CustomFilter.java
+++ src/main/java/WoWSSSC/config/CustomFilter.java
@@ -1,33 +1,37 @@
-//package WoWSSSC.config;
-//
-//import org.apache.commons.lang3.StringUtils;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * Created by Aesis on 2017. 5. 21..
-// */
-//@Configuration
-//@ComponentScan(basePackages = {"WoWSSSC"})
-//public class CustomFilter implements Filter
-//{
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException
-//    {
-//
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
-//    {
-//        HttpServletRequest request = (HttpServletRequest) req;
-//        HttpServletResponse response = (HttpServletResponse) res;
-//
+package WoWSSSC.config;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Aesis on 2017. 5. 21..
+ */
+@Configuration
+@ComponentScan(basePackages = {"WoWSSSC"})
+public class CustomFilter implements Filter
+{
+    @Autowired
+    private DiscordWebhook discordWebhook;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
 //        StringBuilder https = new StringBuilder("https://");
 //        String serverName = request.getServerName();
 //        String uri = request.getRequestURI();
@@ -56,13 +60,25 @@
 //            response.setHeader("Connection", "close");
 ////            response.sendRedirect(https.toString());
 //        }
-//
-//        chain.doFilter(req, res);
-//    }
-//
-//    @Override
-//    public void destroy()
-//    {
-//
-//    }
-//}
+
+        try
+        {
+            chain.doFilter(req, res);
+        }
+        catch (Exception exception)
+        {
+            try {
+                discordWebhook.sendDiscordWebHook(exception, request);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
+}
