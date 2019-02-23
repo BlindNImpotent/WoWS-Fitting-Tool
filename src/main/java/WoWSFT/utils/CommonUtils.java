5162295db6e1b8c75e3541df47be5d692c96fd92
K Lee <20157185+EdibleBug@users.noreply.github.com>
2019-02-23 12:06:35 +0900

    GameParams - Discord Webhook

--- src/main/java/WoWSFT/utils/CommonUtils.java
+++ src/main/java/WoWSFT/utils/CommonUtils.java
@@ -1,5 +1,11 @@
 package WoWSFT.utils;
 
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+
 import static WoWSFT.model.Constant.*;
 
 public class CommonUtils
@@ -27,5 +33,25 @@ public class CommonUtils
     public static String getNumSym(Number number)
     {
         return (number.floatValue() >= 0 ? "+" : "") + replaceZero(number.toString());
+    }
+
+    public static void sendDiscordWH(String message) throws IOException
+    {
+        String content = "{\"content\": \"```java\\n" + message + "\\n```\"}";
+
+        URL url =  new URL("https://discordapp.com/api/webhooks/");
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.addRequestProperty("Content-Type", "application/json; charset=utf-8");
+        connection.addRequestProperty("User-Agent", "WoWSFT Webhook");
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+
+        OutputStream os = connection.getOutputStream();
+        os.write(content.getBytes());
+        os.flush();
+        os.close();
+
+        connection.disconnect();
     }
 }
